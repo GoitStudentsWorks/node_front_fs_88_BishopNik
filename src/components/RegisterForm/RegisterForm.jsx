1f4a6351289fd83b8e4e4f84343e1026172e2cf7
Andrew <a.nikanorov@gmail.com>
2024-02-01 09:15:12 +0100

    clear

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -10,7 +10,6 @@ import PersonAddIcon from '@mui/icons-material/PersonAdd';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Loader from 'components/Loader';
 
 const defaultTheme = createTheme();
 
