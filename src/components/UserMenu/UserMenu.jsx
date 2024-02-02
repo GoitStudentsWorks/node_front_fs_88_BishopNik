6f141e4cef071d6fb3605231b69b82678678d7dd
Andrew <a.nikanorov@gmail.com>
2024-02-03 00:15:29 +0100

    Update UserMenu.jsx

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,31 +1,14 @@
 /** @format */
 
 import { NavLink } from 'react-router-dom';
-import ModalWindow from '../Modal';
-import { useState } from 'react';
 
 export const UserMenu = () => {
-	const [modalIsOpen, setModalIsOpen] = useState(false);
-
-	const closeModal = () => setModalIsOpen(false);
 	return (
 		<>
 			<nav>
 				<NavLink to='/todos' end>
 					Boards
 				</NavLink>
-				<button
-					id='avatar_menu'
-					onClick={() => setModalIsOpen(true)}
-					style={{
-						marginLeft: '30px',
-						width: '100px',
-						height: '25px',
-					}}
-				>
-					Click me
-				</button>
-				{modalIsOpen && <ModalWindow closeModal={closeModal} />}
 			</nav>
 		</>
 	);
