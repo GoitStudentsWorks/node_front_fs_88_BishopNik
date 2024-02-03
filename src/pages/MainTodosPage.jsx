a1fb5d2d4af91227c1e8532098408bacd5acd9e8
evgen122 <evg.khristenko@gmail.com>
2024-02-03 22:20:25 +0200

    moved from the start page to the mainTodos page

--- src/pages/MainTodosPage.jsx
+++ src/pages/MainTodosPage.jsx
@@ -7,39 +7,41 @@ import { resetError } from 'redux/todos/todosSlice';
 import { toastError } from 'components/Helpers';
 import MainComponent from 'components/MainComponent/MainComponent';
 import {
-	Container,
-	Main,
-	SideBar,
-	Header,
+  Container,
+  Main,
+  SideBar,
+  Header,
 } from '../components/styled.component/MainTodosPage.styled';
+import { UserInfo } from 'components/UserInfo/UserInfo';
+import { ModalEditProfile } from 'components/Modal/ModalEditProfile';
 
 // import HeaderComponent from 'components/Header';
 
 function MainTodosPage() {
-	const error = useSelector(statusError);
-	const dispatch = useDispatch();
+  const error = useSelector(statusError);
+  const dispatch = useDispatch();
 
-	useEffect(() => {
-		if (error !== null) toastError(`Error loading data: ${error}`);
-		dispatch(resetError());
-	}, [dispatch, error]);
+  useEffect(() => {
+    if (error !== null) toastError(`Error loading data: ${error}`);
+    dispatch(resetError());
+  }, [dispatch, error]);
 
-	return (
-		<>
-
-			<Container>
-				<SideBar>Header</SideBar>
-				<Header>
-					Header
-					{/* <HeaderComponent /> */}
-				</Header>
-				<Main>
-					<MainComponent>{/* <h2>Sey Hello</h2> */}</MainComponent>
-				</Main>
-			</Container>
-
-		</>
-	);
+  return (
+    <>
+      <Container>
+        <SideBar>Header</SideBar>
+        <Header>
+          Header
+          {/* <HeaderComponent /> */}
+          <UserInfo />
+          <ModalEditProfile />
+        </Header>
+        <Main>
+          <MainComponent>{/* <h2>Sey Hello</h2> */}</MainComponent>
+        </Main>
+      </Container>
+    </>
+  );
 }
 
 export default MainTodosPage;
--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -4,15 +4,10 @@ import { AuthNavigation } from 'components/AuthNavigation';
 
 import React from 'react';
 
-import { ModalEditProfile } from 'components/Modal/ModalEditProfile';
-import { UserInfo } from 'components/UserInfo/UserInfo';
-
 const StartPage = () => {
   return (
     <>
       <AuthNavigation />
-      <UserInfo />
-      <ModalEditProfile />
     </>
   );
 };
