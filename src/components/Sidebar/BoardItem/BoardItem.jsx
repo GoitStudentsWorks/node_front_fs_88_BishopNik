934533cba97e5e25d40e236482092b504215ece9
Andrew <a.nikanorov@gmail.com>
2024-02-04 18:10:09 +0100

    Update BoardItem.jsx

--- src/components/Sidebar/BoardItem/BoardItem.jsx
+++ src/components/Sidebar/BoardItem/BoardItem.jsx
@@ -4,19 +4,19 @@ import {} from './BoardItem.styled';
 
 export const BoardItem = () => {
   return (
-    <HelpContainer>
-      <HelpImgDiv>
-        <img src={data.help} alt="Cactus" width={54} />
-      </HelpImgDiv>
-      <HelpText>
-        If you need help with <HelpSpanText>TaskPro</HelpSpanText>, check out
-        our support resources or reach out to our customer support team.
-      </HelpText>
-      <BtnHelp onClick={() => setIsOpen(true)}>
-        <HelpIcon name="help" />
-        <HelpBtnText>Need help?</HelpBtnText>
-      </BtnHelp>
-      <ModalNeedHelp isOpen={isOpen} setIsOpen={setIsOpen} />
-    </HelpContainer>
+    // <HelpContainer>
+    //   <HelpImgDiv>
+    //     <img src={data.help} alt="Cactus" width={54} />
+    //   </HelpImgDiv>
+    //   <HelpText>
+    //     If you need help with <HelpSpanText>TaskPro</HelpSpanText>, check out
+    //     our support resources or reach out to our customer support team.
+    //   </HelpText>
+    //   <BtnHelp onClick={() => setIsOpen(true)}>
+    //     <HelpIcon name="help" />
+    //     <HelpBtnText>Need help?</HelpBtnText>
+    //   </BtnHelp>
+    //   <ModalNeedHelp isOpen={isOpen} setIsOpen={setIsOpen} />
+    // </HelpContainer>
   );
 };
