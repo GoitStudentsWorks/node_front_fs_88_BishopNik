1db4e3677619a3fb000f67e5d0c7b5e53fa9d8d5
evgen122 <evg.khristenko@gmail.com>
2024-02-05 15:53:27 +0200

    add validation

--- src/components/Modal/ModalEditProfile.js
+++ src/components/Modal/ModalEditProfile.js
@@ -1,35 +1,37 @@
 /** @format */
 
+import { UserProfileForm } from 'components/UserProfileForm/UserProfileForm';
 import Modal from 'react-modal';
 
 Modal.setAppElement('#modal-root');
 
 const customStyles = {
-	overlay: {
-		backgroundColor: 'rgba(78, 74, 72, 0.6)',
-	},
-	content: {
-		top: '50%',
-		left: '50%',
-		right: 'auto',
-		bottom: 'auto',
-		marginRight: '-50%',
-		transform: 'translate(-50%, -50%)',
-	},
+  overlay: {
+    backgroundColor: 'rgba(78, 74, 72, 0.6)',
+  },
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
 };
 
 export const ModalEditProfile = ({ isModalState, stateСhangeModal }) => {
-	return (
-		<Modal
-			isOpen={isModalState}
-			onRequestClose={stateСhangeModal}
-			style={customStyles}
-			contentLabel='onRequestClose'
-		>
-			<p>I am Modal Window</p>
-			<button type='button' onClick={stateСhangeModal}>
-				Close
-			</button>
-		</Modal>
-	);
+  return (
+    <Modal
+      isOpen={isModalState}
+      onRequestClose={stateСhangeModal}
+      style={customStyles}
+      contentLabel="onRequestClose"
+    >
+      <p>I am Modal Window</p>
+      <button type="button" onClick={stateСhangeModal}>
+        Close
+      </button>
+      <UserProfileForm />
+    </Modal>
+  );
 };
---
+++ src/components/UserProfileForm/UserProfileForm.js
@@ -0,0 +1,84 @@
+// import { StyledForm } from 'components/LoginForm/LoginForm.Styled';
+// import { Formik,Field,Form } from 'formik';
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+const emailRegex = RegExp(
+  /^[A-Z|a-z0-9!#$%&._%+-/=?^]+@[A-Z|a-z0-9.-]+\.[A-Z|a-z]{2,4}$/
+);
+
+const passwordRegex = RegExp(/^[A-Z|a-z0-9!#$%&._%+-/=?^]{8,64}$/);
+
+const formShema = Yup.object().shape({
+  login: Yup.string()
+    .min(2, 'Too short!')
+    .max(32, 'Too Long!')
+    .required('This field is required!'),
+
+  email: Yup.string()
+    .matches(emailRegex, 'Invalid email')
+    .email('Invalid email')
+    .required('email is required'),
+
+  password: Yup.string()
+    .matches(passwordRegex, 'Invalid password')
+    .min(8, 'Password should be of minimum 8 characters length')
+    .max(64, 'Password should be of max 64 characters length')
+    .required('This field is required!'),
+});
+
+export const UserProfileForm = () => {
+  return (
+    <Formik
+      initialValues={{
+        login: '',
+        email: '',
+        password: '',
+      }}
+      validationSchema={formShema}
+      onSubmit={async values => {
+        // await new Promise(r => setTimeout(r, 500));
+        alert(JSON.stringify(values, null, 2));
+      }}
+    >
+      {/* <StyledForm>
+        <label htmlFor="name">Name</label>
+        <StyledInput id="name" name="name" />
+        <ErrorMessage name="name" />
+
+        <label htmlFor="number">Number</label>
+        <StyledInput id="number" name="number" placeholder="111-11-11" />
+        <ErrorMessage name="number" />
+
+        <ButAdd type="submit">Add contact</ButAdd>
+      </StyledForm> */}
+      <Form>
+        <label htmlFor="login"></label>
+        <Field id="login" name="login" placeholder="Ivetta" />
+        <ErrorMessage name="login" />
+
+        <label htmlFor="email"></label>
+        <Field
+          id="email"
+          name="email"
+          placeholder="ivetta34@gmail.com"
+          type="email"
+        />
+        <ErrorMessage name="email" />
+
+        <label htmlFor="password"></label>
+        <Field
+          //   type="password"
+          type="text"
+          id="password"
+          name="password"
+          placeholder="ivetta1999.23"
+        />
+        <ErrorMessage name="password" />
+
+        <button type="submit">Send</button>
+      </Form>
+    </Formik>
+  );
+};
