cd2edbaa8011a2b3a81ad2dc8d3da591221e3af7
evgen122 <evg.khristenko@gmail.com>
2024-02-06 18:35:19 +0200

    1

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -14,18 +14,8 @@ import {
   Link,
   NavBox,
   StyledField,
+  StyledFieldContainer,
   StyledForm,
-	Button,
-	ButtonText,
-	Container,
-	ErrMessageStyled,
-	IconHideShow,
-	LabelBox,
-	Link,
-	NavBox,
-	StyledField,
-	StyledFieldContainer,
-	StyledForm,
 } from './LoginForm.Styled';
 import { useState } from 'react';
 
@@ -51,8 +41,8 @@ const LoginForm = () => {
       >
         <StyledForm autoComplete="off">
           <NavBox>
-            <Link to="/register">Registation</Link>
-            <Link to="/login" end>
+            <Link to="/auth/register">Registation</Link>
+            <Link to="/auth/login" end>
               Log In
             </Link>
           </NavBox>
@@ -61,72 +51,24 @@ const LoginForm = () => {
               <StyledField name="email" type="email" placeholder="Email" />
               <ErrMessageStyled name="email" component="span" />
             </label>
-            <label style={{ position: 'relative' }}>
-              <StyledField
-                name="password"
-                type={showPassword ? 'text' : 'password'}
-                placeholder="Create a password"
-              />
-              <IconHideShow
-                name="hide-show"
-                onClick={togglePasswordVisibility}
-                style={{
-                  position: 'absolute',
-                  top: '50%',
-                  right: '18px',
-                  transform: 'translateY(-50%)',
-                  cursor: 'pointer',
-                  stroke: '#FFFFFF4D',
-                }}
-              >
-                {showPassword ? 'Hide' : 'Show'}
-              </IconHideShow>
+            <label>
+              <StyledFieldContainer>
+                <StyledField
+                  name="password"
+                  type={showPassword ? 'text' : 'password'}
+                  placeholder="Create a password"
+                />
+                <IconHideShow
+                  name="hide-show"
+                  onClick={togglePasswordVisibility}
+                >
+                  {showPassword ? 'Hide' : 'Show'}
+                </IconHideShow>
+              </StyledFieldContainer>
+
               <ErrMessageStyled name="password" component="span" />
             </label>
           </LabelBox>
-	const togglePasswordVisibility = () => {
-		setShowPassword(!showPassword);
-	};
-	const handleSubmit = (values, actions) => {
-		dispatch(logIn(values));
-		actions.resetForm({ email: '', password: '' });
-	};
-	return (
-		<Container>
-			<Formik
-				initialValues={{ email: '', password: '' }}
-				onSubmit={(values, actions) => {
-					handleSubmit(values, actions);
-				}}
-				validationSchema={LoginSchema}
-			>
-				<StyledForm autoComplete='off'>
-					<NavBox>
-						<Link to='/auth/register'>Registation</Link>
-						<Link to='/auth/login' end>
-							Log In
-						</Link>
-					</NavBox>
-					<LabelBox>
-						<label>
-							<StyledField name='email' type='email' placeholder='Email' />
-							<ErrMessageStyled name='email' component='span' />
-						</label>
-						<label>
-							<StyledFieldContainer>
-								<StyledField
-									name='password'
-									type={showPassword ? 'text' : 'password'}
-									placeholder='Create a password'
-								/>
-								<IconHideShow name='hide-show' onClick={togglePasswordVisibility}>
-									{showPassword ? 'Hide' : 'Show'}
-								</IconHideShow>
-							</StyledFieldContainer>
-
-							<ErrMessageStyled name='password' component='span' />
-						</label>
-					</LabelBox>
 
           <Button type="submit">
             <ButtonText>Log In Now</ButtonText>
