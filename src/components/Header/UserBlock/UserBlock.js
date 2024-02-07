251f31943fc9918c1d612a51321e5c57f98b3903
Andrew <a.nikanorov@gmail.com>
2024-02-07 16:21:52 +0100

    fix UserBlock

--- src/components/Header/UserBlock/UserBlock.js
+++ src/components/Header/UserBlock/UserBlock.js
@@ -1,23 +1,22 @@
+/** @format */
 
 import { useSelector } from 'react-redux';
-import {UserName, Wrapper } from './UserBlock.styled';
+import { UserName, Wrapper } from './UserBlock.styled';
 import data from '../../../img/list_img.json';
-import {selectUser} from 'redux/auth/selectors'
+import { selectUser } from 'redux/auth/selectors';
 
 const UserBlock = () => {
-  const { name } = useSelector(selectUser);
-  console.log(name)
+	const { name } = useSelector(selectUser);
 
-  
-
-  return (
-    <> <Wrapper>
-      <UserName>{name[0].toUpperCase() + name.slice(1)}</UserName>
-          <img src={data.user.dark} alt="DarkUser" width={32} />
-    </Wrapper>
-    </>
-     
-  );
+	return (
+		<>
+			{' '}
+			<Wrapper>
+				<UserName>{name[0].toUpperCase() + name.slice(1)}</UserName>
+				<img src={data.user.dark} alt='DarkUser' width={32} />
+			</Wrapper>
+		</>
+	);
 };
 
 export default UserBlock;
