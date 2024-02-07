959c7e7249762a362ad497955bbb779e49172f91
YuliiaKush <128642386+YuliiaKush@users.noreply.github.com>
2024-02-07 21:23:33 +0200

    update

--- src/components/Header/UserBlock/UserBlock.js
+++ src/components/Header/UserBlock/UserBlock.js
@@ -2,22 +2,34 @@
 
 import { useSelector } from 'react-redux';
 import { UserName, Wrapper, UserAvatar } from './UserBlock.styled';
-import UserPhoto from '../../../img/user.jpg';
-import { selectUser } from 'redux/auth/selectors';
+import UserDark from '../../../img/userDark.jpg';
+import UserLight from '../../../img/userLight.jpg'
+import UserViolet from '../../../img/userViolet.jpg'
+import { selectUser, selectUserTheme } from 'redux/auth/selectors';
 
 const UserBlock = () => {
 	const { name } = useSelector(selectUser);
+	const activeUserTheme = useSelector(selectUserTheme);
 
+	 const setDefaultAvatar = () => {
+    if (activeUserTheme === 'dark') {
+      return UserDark;
+    } else if (activeUserTheme === 'light') {
+      return UserLight;
+    } else if (activeUserTheme === 'violet') {
+      return UserViolet;
+    }
+	};
+	
 	return (
 		<>
 			{' '}
 			<Wrapper>
 				<UserName>{name[0].toUpperCase() + name.slice(1)}</UserName>
 				<UserAvatar
-            src={UserPhoto()}
-            alt="user name"
-
-          />
+					src={UserLight || setDefaultAvatar }
+					alt="user name"
+				/>
 			</Wrapper>
 		</>
 	);
