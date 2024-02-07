6c3715da8ba792e18bc2456eb30d691efdccf75a
Andrew <a.nikanorov@gmail.com>
2024-02-08 00:10:16 +0100

    fix AddBoardModal

--- src/components/Helpers/ModalSchemas.js
+++ src/components/Helpers/ModalSchemas.js
@@ -1,14 +1,14 @@
+/** @format */
+
 import * as Yup from 'yup';
 
 export const needHelpSchema = Yup.object().shape({
-  email: Yup.string()
-    .email('Invalid email address')
-    .required('This is a required field'),
-  comment: Yup.string().required('This is a required field'),
+	email: Yup.string().email('Invalid email address').required('This is a required field'),
+	comment: Yup.string().required('This is a required field'),
 });
 
 export const createBoardSchema = Yup.object().shape({
-	title: Yup.string().required('This is a required field'),
+	name: Yup.string().required('This is a required field'),
 	icon: Yup.string().required('This is a required field'),
 	background: Yup.string().required('This is a required field'),
 });
--- src/components/Modal/CreateNewBoardModal/CreateNewBoardModal.js
+++ src/components/Modal/CreateNewBoardModal/CreateNewBoardModal.js
@@ -3,24 +3,24 @@
 import { Formik, Field } from 'formik';
 import { createBoardSchema } from '../../Helpers/ModalSchemas';
 import {
-  StyledForm,
-  HeaderContainer,
-  CloseIcon,
-  LabelBox,
-  StyledField,
-  Button,
-  ButtonText,
-  Title,
-  TitleIcons,
-  IconsContainer,
-  TypesOfIcon,
-  TitleBackground,
-  BackgroundContainer,
-  TypesOfBackground,
-  LabelRadio,
-  IconWrapper,
-  AddIcon,
-  ErrMessageStyled,
+	StyledForm,
+	HeaderContainer,
+	CloseIcon,
+	LabelBox,
+	StyledField,
+	Button,
+	ButtonText,
+	Title,
+	TitleIcons,
+	IconsContainer,
+	TypesOfIcon,
+	TitleBackground,
+	BackgroundContainer,
+	TypesOfBackground,
+	LabelRadio,
+	IconWrapper,
+	AddIcon,
+	ErrMessageStyled,
 } from './CreateNewBoardModal.styled';
 import { customStyles } from '../Modal.styled';
 import { addBoard } from 'redux/boards/operations';
@@ -30,101 +30,88 @@ import { useDispatch, useSelector } from 'react-redux';
 import { boardsSlice } from 'redux/boards/boardsSlice';
 import { modalData, boardsState } from 'redux/boards/selectors';
 
-const icons = [1, 2, 3, 4, 5, 6, 7, 8];
-const backgrounds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-
 export const CreateNewBoardModal = () => {
-  const dispatch = useDispatch();
-  const { isOpen, boardId } = useSelector(modalData);
-  const boards = useSelector(boardsState);
+	const dispatch = useDispatch();
+	const { isOpen, boardId } = useSelector(modalData);
+	const boards = useSelector(boardsState);
 
-  const boardForEditing = boardId && boards.find(item => item.id === boardId);
+	const boardForEditing = boardId && boards.find(item => item.id === boardId);
 
-  const closeModal = () => {
-    dispatch(boardsSlice.actions.openCreateEditBoardModal({ isOpen: false }));
-  };
+	const closeModal = () => {
+		dispatch(boardsSlice.actions.openCreateEditBoardModal({ isOpen: false }));
+	};
 
-  return (
-    <ModalWindow
-      isOpen={isOpen}
-      onRequestClose={closeModal}
-      style={customStyles}
-    >
-      <Formik
-        initialValues={{
-          title: boardForEditing?.name || '',
-          icon: boardForEditing?.icon || '',
-          background: boardForEditing?.background || '',
-          id: boardId,
-        }}
-        onSubmit={(values, actions) => {
-          console.log(values);
+	return (
+		<ModalWindow isOpen={isOpen} onRequestClose={closeModal} style={customStyles}>
+			<Formik
+				initialValues={{
+					name: boardForEditing?.name || '',
+					icon: boardForEditing?.icon || '',
+					background: boardForEditing?.background || '',
+					id: boardId,
+				}}
+				onSubmit={(board, actions) => {
+					if (boardId) {
+						dispatch(addBoard(board));
+					} else {
+						dispatch(addBoard(board));
+					}
+				}}
+				validationSchema={createBoardSchema}
+			>
+				<StyledForm autoComplete='off'>
+					<HeaderContainer>
+						<Title>New board</Title>
+						<CloseIcon name='close' onClick={closeModal} />
+					</HeaderContainer>
 
-          const data = {
-            name: values.title,
-            icon: values.icon,
-          };
-          if (boardId) {
-            dispatch(addBoard(data));
-          } else {
-            dispatch(addBoard(data));
-          }
-        }}
-        validationSchema={createBoardSchema}
-      >
-        <StyledForm autoComplete="off">
-          <HeaderContainer>
-            <Title>New board</Title>
-            <CloseIcon name="close" onClick={closeModal} />
-          </HeaderContainer>
+					<LabelBox>
+						<label>
+							<StyledField name='name' type='text' placeholder='Title' />
+							<ErrMessageStyled name='name' component='span' />
+						</label>
+					</LabelBox>
+					<TitleIcons>Icons</TitleIcons>
 
-          <LabelBox>
-            <label>
-              <StyledField name="title" type="text" placeholder="Title" />
-              <ErrMessageStyled name="title" component="span" />
-            </label>
-          </LabelBox>
-          <TitleIcons>Icons</TitleIcons>
+					<IconsContainer>
+						{Array.from({ length: 8 }, (_, iconIndex) => (
+							<LabelRadio key={`icons-${iconIndex}`}>
+								<Field
+									className='invisible'
+									type='radio'
+									name='icon'
+									value={iconIndex}
+								/>
+								<TypesOfIcon name={`type-${iconIndex}`} />
+							</LabelRadio>
+						))}
+					</IconsContainer>
 
-          <IconsContainer>
-            {icons.map(iconIndex => (
-              <LabelRadio key={`icons-${iconIndex}`}>
-                <Field
-                  className="invisible"
-                  type="radio"
-                  name="icon"
-                  value={iconIndex}
-                />
-                <TypesOfIcon name={`type-${iconIndex}`} />
-              </LabelRadio>
-            ))}
-          </IconsContainer>
+					<TitleBackground>Background</TitleBackground>
+					<BackgroundContainer>
+						{Array.from({ length: 15 }, (_, imageIndex) => (
+							<LabelRadio key={`background-${imageIndex}`}>
+								<Field
+									className='invisible'
+									type='radio'
+									name='background'
+									value={imageIndex}
+								/>
+								<TypesOfBackground
+									src={background.mobile[`image${imageIndex}`]}
+								></TypesOfBackground>
+							</LabelRadio>
+						))}
+					</BackgroundContainer>
 
-          <TitleBackground>Background</TitleBackground>
-          <BackgroundContainer>
-            {backgrounds.map(imageIndex => (
-              <LabelRadio key={`backround-${imageIndex}`}>
-                <Field
-                  className="invisible"
-                  type="radio"
-                  name="background"
-                  value={imageIndex}
-                />
-                <TypesOfBackground
-                  src={background.mobile[`image${imageIndex}`]}
-                ></TypesOfBackground>
-              </LabelRadio>
-            ))}
-          </BackgroundContainer>
-
-          <Button type="submit">
-            <IconWrapper>
-              <AddIcon name="add-board" />
-            </IconWrapper>
-            <ButtonText>Create</ButtonText>
-          </Button>
-        </StyledForm>
-      </Formik>
-    </ModalWindow>
-  );
+					<Button type='submit'>
+						<IconWrapper>
+							<AddIcon name='add-board' />
+						</IconWrapper>
+						<ButtonText>Create</ButtonText>
+					</Button>
+				</StyledForm>
+			</Formik>
+		</ModalWindow>
+	);
 };
