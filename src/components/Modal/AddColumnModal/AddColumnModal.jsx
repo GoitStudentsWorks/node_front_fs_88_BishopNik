1637013289ee72f19cae30c7eac70d82ec197c6c
Andrew <a.nikanorov@gmail.com>
2024-02-08 21:40:01 +0100

    fix AddColonmModal

--- src/components/Modal/AddColumnModal/AddColumnModal.jsx
+++ src/components/Modal/AddColumnModal/AddColumnModal.jsx
@@ -1,66 +1,62 @@
 /** @format */
 
-import React, { useState } from 'react';
+import React from 'react';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
 import ModalWindow from '../Modal';
 import { customStyles } from '../Modal.styled';
 
 import {
-  ModalTitle,
-  StyledForm,
-  StyledFormField,
-  StyledErrorMessage,
-  BtnAdd,
-  HeaderContainer,
-  CloseIcon,
-  IconWrapper,
-  AddIcon,
+	ModalTitle,
+	StyledForm,
+	StyledFormField,
+	StyledErrorMessage,
+	BtnAdd,
+	HeaderContainer,
+	CloseIcon,
+	IconWrapper,
+	AddIcon,
 } from './AddColumnModal.styled';
 import { useDispatch } from 'react-redux';
 import { addColum } from 'redux/columns/operations';
 
 const columnSchema = Yup.object().shape({
-  name: Yup.string().required('name is required'),
+	name: Yup.string().required('name is required'),
 });
 
 export const AddColumnModal = ({ isOpen, setIsOpen, board }) => {
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-  return (
-    <ModalWindow
-      isOpen={isOpen}
-      onRequestClose={() => setIsOpen(false)}
-      style={customStyles}
-    >
-      <Formik
-        initialValues={{
-          name: '',
-          boarderId: `${board}`,
-        }}
-        validationSchema={columnSchema}
-        onSubmit={(newColumn, actions) => {
-          dispatch(addColum(newColumn));
-        }}
-      >
-        {({ isSubmitting }) => (
-          <StyledForm autoComplete="off">
-            <HeaderContainer>
-              <ModalTitle>Add column</ModalTitle>
-              <CloseIcon name="close" onClick={() => setIsOpen(false)} />
-            </HeaderContainer>
+	return (
+		<ModalWindow isOpen={isOpen} onRequestClose={() => setIsOpen(false)} style={customStyles}>
+			<Formik
+				initialValues={{
+					name: '',
+					boarderId: `${board}`,
+				}}
+				validationSchema={columnSchema}
+				onSubmit={(newColumn, actions) => {
+					dispatch(addColum(newColumn));
+				}}
+			>
+				{({ isSubmitting }) => (
+					<StyledForm autoComplete='off'>
+						<HeaderContainer>
+							<ModalTitle>Add column</ModalTitle>
+							<CloseIcon name='close' onClick={() => setIsOpen(false)} />
+						</HeaderContainer>
 
-            <StyledFormField type="text" name="name" placeholder="Title" />
-            <StyledErrorMessage name="name" component="div" />
-            <BtnAdd type="submit" disabled={isSubmitting}>
-              <IconWrapper>
-                <AddIcon name="add-board" />
-              </IconWrapper>
-              Add
-            </BtnAdd>
-          </StyledForm>
-        )}
-      </Formik>
-    </ModalWindow>
-  );
+						<StyledFormField type='text' name='name' placeholder='Title' />
+						<StyledErrorMessage name='name' component='div' />
+						<BtnAdd type='submit' disabled={isSubmitting}>
+							<IconWrapper>
+								<AddIcon name='add-board' />
+							</IconWrapper>
+							Add
+						</BtnAdd>
+					</StyledForm>
+				)}
+			</Formik>
+		</ModalWindow>
+	);
 };
