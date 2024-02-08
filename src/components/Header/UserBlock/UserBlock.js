e82beaf26a26cc7eb67750e407465d7ca8b699ff
YuliiaKush <128642386+YuliiaKush@users.noreply.github.com>
2024-02-08 18:55:05 +0200

    delate UserBlock

--- src/components/Header/UserBlock/UserBlock.js
+++ src/components/Header/UserBlock/UserBlock.js
@@ -1,38 +1,39 @@
-/** @format */
+// /** @format */
 
-import { useSelector } from 'react-redux';
-import { UserName, Wrapper, UserAvatar } from './UserBlock.styled';
-import UserDark from '../../../img/userDark.jpg';
-import UserLight from '../../../img/userLight.jpg'
-import UserViolet from '../../../img/userViolet.jpg'
-import { selectUser, selectUserTheme } from 'redux/auth/selectors';
-
-const UserBlock = () => {
-	const { name } = useSelector(selectUser);
-	const activeUserTheme = useSelector(selectUserTheme);
-
-	 const setDefaultAvatar = () => {
-    if (activeUserTheme === 'dark') {
-      return UserDark;
-    } else if (activeUserTheme === 'light') {
-      return UserLight;
-    } else if (activeUserTheme === 'violet') {
-      return UserViolet;
-    }
-	};
+// import { useSelector } from 'react-redux';
+// import { UserName, Wrapper, UserAvatar } from './UserBlock.styled';
+// import UserDark from '../../../img/userDark.jpg';
+// import UserLight from '../../../img/userLight.jpg'
+// import UserViolet from '../../../img/userViolet.jpg'
+// import { selectUser, selectUserTheme } from 'redux/auth/selectors';
+// import UserInfo from '../../../components/UserInfo/UserInfo'
 	
-	return (
-		<>
-			{' '}
-			<Wrapper>
-				<UserName>{name[0].toUpperCase() + name.slice(1)}</UserName>
-				<UserAvatar
-					src={UserLight || setDefaultAvatar }
-					alt="user name"
-				/>
-			</Wrapper>
-		</>
-	);
-};
+// import  UserBlock = () => {
+// 	const { name } = useSelector(selectUser);
+// 	const activeUserTheme = useSelector(selectUserTheme);
 
-export default UserBlock;
+// 	 const setDefaultAvatar = () => {
+//     if (activeUserTheme === 'dark') {
+//       return UserDark;
+//     } else if (activeUserTheme === 'light') {
+//       return UserLight;
+//     } else if (activeUserTheme === 'violet') {
+//       return UserViolet;
+//     }
+// 	};
+	
+// 	return (
+// 		<>
+// 			{' '}
+// 			<Wrapper>
+// 				<UserName>{name[0].toUpperCase() + name.slice(1)}</UserName>
+// 				<UserAvatar
+// 					src={UserLight || setDefaultAvatar }
+// 					alt="user name"
+// 				/>
+// 			</Wrapper>
+// 		</>
+// 	);
+// };
+
+// export default UserBlock;
