e42afddb7c776d91b8beefc4b6f1e4eacc87a599
Andrew <a.nikanorov@gmail.com>
2024-02-10 15:35:45 +0100

    fix Filter & Column

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,76 +1,79 @@
 /** @format */
 
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
+import { MainContext } from 'components/Helpers';
 import {
-  CloseButton,
-  IconClose,
-  LinkShowAll,
-  ModalContainer,
-  RadioButton,
-  RadioButtonGroup,
-  Title,
-  TitleBox,
-  TitleRadioButtons,
+	CloseButton,
+	IconClose,
+	LinkShowAll,
+	ModalContainer,
+	RadioButton,
+	RadioButtonGroup,
+	Title,
+	TitleBox,
+	TitleRadioButtons,
 } from './Filter.styled.jsx';
 
 export const Filter = ({ setIsOpen }) => {
-  const [selectedOption, setSelectedOption] = useState('Without priority');
+	const { filter, setFilter } = useContext(MainContext);
+	const [selectedOption, setSelectedOption] = useState(filter);
 
-  const handleOptionChange = e => {
-    setSelectedOption(e.target.value);
-  };
+	const handleOptionChange = ({ target }) => {
+		setSelectedOption(target.value);
+		setFilter(target.value);
+	};
 
-  return (
-    <ModalContainer>
-      <TitleBox>
-        <Title>Filters</Title>
-      </TitleBox>
+	return (
+		<ModalContainer>
+			<TitleBox>
+				<Title>Filters</Title>
+			</TitleBox>
 
-      <CloseButton type="button" onClick={() => setIsOpen(false)}>
-        <IconClose name='close'/>
-      </CloseButton>
-	  <div>
-		<LinkShowAll href="URL">Show all</LinkShowAll>
-	  </div>
-      <TitleRadioButtons>Label color</TitleRadioButtons>
-      <RadioButtonGroup>
-        <RadioButton>
-          <input
-            type="radio"
-            value="Without priority"
-            checked={selectedOption === 'Without priority'}
-            onChange={handleOptionChange}
-          />
-          Without priority
-        </RadioButton>
-        <RadioButton>
-          <input
-            type="radio"
-            value="Low"
-            checked={selectedOption === 'Low'}
-            onChange={handleOptionChange}
-          />
-          Low
-        </RadioButton>
-        <RadioButton>
-          <input
-            type="radio"
-            value="Medium"
-            checked={selectedOption === 'Medium'}
-            onChange={handleOptionChange}
-          />
-          Medium
-        </RadioButton>
-        <RadioButton>
-          <input
-            type="radio"
-            value="High"
-            checked={selectedOption === 'High'}
-            onChange={handleOptionChange}
-          />
-          High
-        </RadioButton>
-      </RadioButtonGroup>
-    </ModalContainer>
-  );
+			<CloseButton type='button' onClick={() => setIsOpen(false)}>
+				<IconClose name='close' />
+			</CloseButton>
+			<div>
+				<LinkShowAll href='URL'>Show all</LinkShowAll>
+			</div>
+			<TitleRadioButtons>Label color</TitleRadioButtons>
+			<RadioButtonGroup>
+				<RadioButton>
+					<input
+						type='radio'
+						value='without'
+						checked={selectedOption === 'without'}
+						onChange={handleOptionChange}
+					/>
+					Without priority
+				</RadioButton>
+				<RadioButton>
+					<input
+						type='radio'
+						value='Low'
+						checked={selectedOption === 'Low'}
+						onChange={handleOptionChange}
+					/>
+					Low
+				</RadioButton>
+				<RadioButton>
+					<input
+						type='radio'
+						value='Medium'
+						checked={selectedOption === 'Medium'}
+						onChange={handleOptionChange}
+					/>
+					Medium
+				</RadioButton>
+				<RadioButton>
+					<input
+						type='radio'
+						value='High'
+						checked={selectedOption === 'High'}
+						onChange={handleOptionChange}
+					/>
+					High
+				</RadioButton>
+			</RadioButtonGroup>
+		</ModalContainer>
+	);
 };
--- src/components/styled.component/MainTodosPage.styled.jsx
+++ src/components/styled.component/MainTodosPage.styled.jsx
@@ -3,75 +3,89 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  margin-left: auto;
-  margin-right: auto;
+	margin-left: auto;
+	margin-right: auto;
 
-  display: grid;
-  grid-template-columns: 1fr;
-  grid-template-areas:
-    'h'
-    'm';
+	display: grid;
+	grid-template-columns: 1fr;
+	grid-template-areas:
+		'h'
+		'm';
 
-  @media screen and (min-width: 375px) {
-    width: 375px;
-  }
+	@media screen and (min-width: 375px) {
+		width: 375px;
+	}
 
-  @media screen and (min-width: 768px) {
-    width: 768px;
-  }
+	@media screen and (min-width: 768px) {
+		width: 768px;
+	}
 
-  @media screen and (min-width: 1440px) {
-    display: grid;
-    grid-template-columns: 260px auto;
-    grid-template-areas:
-      's h'
-      's m';
-    width: 1440px;
-  }
+	@media screen and (min-width: 1440px) {
+		display: grid;
+		grid-template-columns: 260px auto;
+		grid-template-areas:
+			's h'
+			's m';
+		width: 1440px;
+	}
 `;
 
 export const SideBar = styled.aside`
-  grid-area: s;
-  width: 225px;
-  height: 100vh;
-  min-height: 770px;
-  background-color: ${props => props.theme.sidebar.background};
-  display: flex;
-  flex-direction: column;
+	grid-area: s;
+	width: 225px;
+	height: 100vh;
+	min-height: 770px;
+	background-color: ${props => props.theme.sidebar.background};
+	display: flex;
+	flex-direction: column;
 
-  @media screen and (min-width: 768px) {
-    width: 260px;
-  }
+	@media screen and (min-width: 768px) {
+		width: 260px;
+	}
 
-  @media screen and (max-width: 1439px) {
-    /* display: none; */
-    position: absolute;
-    z-index: 9999;
-  }
+	@media screen and (max-width: 1439px) {
+		/* display: none; */
+		position: absolute;
+		z-index: 9999;
+	}
 `;
 
 export const Header = styled.header`
-  grid-area: h;
-  min-width: 320px;
-  height: 60px;
+	grid-area: h;
+	min-width: 320px;
+	height: 60px;
 
-  @media screen and (min-width: 768px) {
-    height: 68px;
-  }
+	@media screen and (min-width: 768px) {
+		height: 68px;
+	}
 `;
 
 export const Main = styled.main`
-  position: relative;
-  grid-area: m;
-  background-color: ${props => props.theme.header.containerBackground};
-  min-width: 320px;
-  height: calc(100vh - 60px);
+	display: flex;
+	flex-direction: column;
+	position: relative;
+	grid-area: m;
+	background-color: ${props => props.theme.header.containerBackground};
+	min-width: 320px;
+	height: calc(100vh - 60px);
+	padding-top: 14px;
+	padding-bottom: 24px;
+	gap: 39px;
 
-  @media screen and (min-width: 375px) {
-    min-height: 702px;
-  }
+	@media screen and (min-width: 375px) {
+		min-height: 702px;
+	}
 
-  @media screen and (min-width: 768px) {
-    max-height: calc(100vh - 68px);
-  }
+	@media screen and (min-width: 768px) {
+		max-height: calc(100vh - 68px);
+		padding-top: 26px;
+		padding-bottom: 32px;
+		gap: 26px;
+	}
+
+	@media screen and (min-width: 1440px) {
+		padding-top: 10px;
+		padding-bottom: 8px;
+		gap: 10px;
+	}
 `;
