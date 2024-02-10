372af41f252f2b13528a8d8d8438c33132c881fb
Irina2304 <110673165+Irina2304@users.noreply.github.com>
2024-02-10 19:08:49 +0100

    scroll
    вертикальный и горизонтальный

--- src/components/Column/Column.jsx
+++ src/components/Column/Column.jsx
@@ -3,23 +3,24 @@
 import React, { useContext, useState, useEffect } from 'react';
 
 import {
-	Wrapper,
-	Title,
-	IconsContainer,
-	EditColumn,
-	DelColumn,
-	List,
-	ListTasks,
+  Wrapper,
+  Title,
+  IconsContainer,
+  EditColumn,
+  DelColumn,
+  List,
+  ListTasks,
+  ListTasksContainer,
 } from './Column.styled';
 import { useSelector, useDispatch } from 'react-redux';
 import { cardsState } from 'redux/cards/selectors';
 import { fetchCardsByColumnId } from 'redux/cards/operations';
 
 import {
-	Button,
-	ButtonText,
-	IconWrapper,
-	AddIcon,
+  Button,
+  ButtonText,
+  IconWrapper,
+  AddIcon,
 } from 'components/Modal/CreateNewBoardModal/CreateNewBoardModal.styled';
 import icon from 'components/Icon/icon-spraite.svg';
 
@@ -29,54 +30,64 @@ import { delColumn } from 'redux/columns/operations';
 import { MainContext } from 'components/Helpers';
 
 export const Column = ({ name, id }) => {
-	const [isOpen, setIsOpen] = useState(false);
-	const cards = useSelector(cardsState);
-	const dispatch = useDispatch();
-	const { filter } = useContext(MainContext);
+  const [isOpen, setIsOpen] = useState(false);
+  const cards = useSelector(cardsState);
+  const dispatch = useDispatch();
+  const { filter } = useContext(MainContext);
 
-	useEffect(() => {
-		dispatch(fetchCardsByColumnId(id));
-	}, [dispatch, id]);
-	const handleDeleteColumn = columnId => {
-		dispatch(delColumn(columnId));
-	};
+  useEffect(() => {
+    dispatch(fetchCardsByColumnId(id));
+  }, [dispatch, id]);
+  const handleDeleteColumn = columnId => {
+    dispatch(delColumn(columnId));
+  };
 
-	const cardForColumn = cards.filter(card => card.columnId === id && card.priority === filter);
-	console.log('🚀 ~ Column ~ cards:', cards);
+  const cardForColumn = cards.filter(
+    card => card.columnId === id && card.priority === filter
+  );
+  console.log('🚀 ~ Column ~ cards:', cards);
 
-	return (
-		<Wrapper>
-			<List>
-				<Title>
-					{name}
-					<IconsContainer>
-						<EditColumn type='button' width='16' height='16'>
-							<use xlinkHref={`${icon}#icon-edit`} />
-						</EditColumn>
-						<DelColumn
-							type='button'
-							width='16'
-							height='16'
-							onClick={() => handleDeleteColumn(id)}
-						>
-							<use xlinkHref={`${icon}#icon-delete`} />
-						</DelColumn>
-					</IconsContainer>
-				</Title>
-			</List>
-			<ListTasks>
-				{cardForColumn.map(item => (
-					<Card key={item._id} item={item} />
-				))}
-			</ListTasks>
-			<Button type='button' onClick={() => setIsOpen(true)}>
-				<IconWrapper>
-					<AddIcon name='add-board' />
-				</IconWrapper>
-				<ButtonText>{!cardForColumn.length ? 'Add card' : 'Add another card'}</ButtonText>
-			</Button>
+  return (
+    <Wrapper>
+      <List>
+        <Title>
+          {name}
+          <IconsContainer>
+            <EditColumn type="button" width="16" height="16">
+              <use xlinkHref={`${icon}#icon-edit`} />
+            </EditColumn>
+            <DelColumn
+              type="button"
+              width="16"
+              height="16"
+              onClick={() => handleDeleteColumn(id)}
+            >
+              <use xlinkHref={`${icon}#icon-delete`} />
+            </DelColumn>
+          </IconsContainer>
+        </Title>
+      </List>
+      <ListTasksContainer>
+        <ListTasks>
+          {cardForColumn.map(item => (
+            <Card key={item._id} item={item} />
+          ))}
+        </ListTasks>
+      </ListTasksContainer>
+      <Button type="button" onClick={() => setIsOpen(true)}>
+        <IconWrapper>
+          <AddIcon name="add-board" />
+        </IconWrapper>
+        <ButtonText>
+          {!cardForColumn.length ? 'Add card' : 'Add another card'}
+        </ButtonText>
+      </Button>
 
-			<AddCardModal isOpen={isOpen} onRequestClose={() => setIsOpen(false)} columnId={id} />
-		</Wrapper>
-	);
+      <AddCardModal
+        isOpen={isOpen}
+        onRequestClose={() => setIsOpen(false)}
+        columnId={id}
+      />
+    </Wrapper>
+  );
 };
--- src/components/Column/Column.styled.jsx
+++ src/components/Column/Column.styled.jsx
@@ -3,68 +3,87 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.li`
-	width: 335px;
-	display: grid;
-	gap: 8px;
-	grid-template-columns: 1fr;
-	grid-template-areas:
-		'title'
-		'task'
-		'add';
-	grid-template-rows: 56px auto 56px;
+  width: 351px;
+  display: grid;
+  gap: 8px;
+  grid-template-columns: 1fr;
+  grid-template-areas:
+    'title'
+    'task'
+    'add';
+  grid-template-rows: 56px auto 56px;
 `;
 
 export const Title = styled.span`
-	color: ${props => props.theme.colum.textMain};
-	font-size: 18px;
-	border-radius: 8px;
-	width: 334px;
-	padding: 14px 24px;
-	background-color: #121212;
-	color: #ffffff;
-	font-size: 14px;
-	font-weight: 500;
-	display: flex;
-	height: 58px;
-	align-items: center;
-	justify-content: space-between;
+  color: ${props => props.theme.colum.textMain};
+  font-size: 18px;
+  border-radius: 8px;
+  width: 334px;
+  padding: 14px 24px;
+  background-color: #121212;
+  color: #ffffff;
+  font-size: 14px;
+  font-weight: 500;
+  display: flex;
+  height: 58px;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 export const List = styled.div`
-	grid-area: title;
-	width: 100%;
-	display: flex;
-	flex-direction: column;
-	margin: 0;
-	padding: 0;
-	gap: 8px;
-	overflow-y: auto;
+  grid-area: title;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin: 0;
+  padding: 0;
+  gap: 8px;
+  overflow-y: auto;
+`;
+
+export const ListTasksContainer = styled.div`
+  overflow-y: scroll;
+  overflow-x: hidden;
+
+  &::-webkit-scrollbar {
+    width: 8px;
+    background: rgba(255, 255, 255, 0.1);
+    border-radius: 4px;
+  }
+  &::-webkit-scrollbar-thumb {
+    height: 142px;
+    background-color: rgba(18, 18, 18, 1);
+    border-radius: 4px;
+    box-shadow: inset 1px 1px 10px rgba(0, 0, 0, 0.25);
+  }
+  &::-webkit-scrollbar-thumb:hover {
+    background-color: ${props => props.theme.sidebar.addButtonBackground};
+  }
 `;
 
 export const ListTasks = styled.div`
-	grid-area: task;
-	width: 100%;
-	display: flex;
-	flex-direction: column;
-	margin: 0;
-	padding: 0;
-	gap: 8px;
-	overflow-y: scroll;
+  grid-area: task;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin: 0;
+  padding: 0;
+  gap: 8px;
 `;
 
 export const IconsContainer = styled.div`
-	grid-area: add;
-	display: flex;
-	gap: 8px;
-	margin-left: auto;
+  grid-area: add;
+  display: flex;
+  gap: 8px;
+  margin-left: auto;
 `;
 
 export const EditColumn = styled.svg`
-	stroke: rgba(255, 255, 255, 0.5);
-	cursor: pointer;
+  stroke: rgba(255, 255, 255, 0.5);
+  cursor: pointer;
 `;
 
 export const DelColumn = styled.svg`
-	stroke: rgba(255, 255, 255, 0.5);
-	cursor: pointer;
+  stroke: rgba(255, 255, 255, 0.5);
+  cursor: pointer;
 `;
