33021c0c1b4f74769c3ddcc394cd95cce941442c
Irina2304 <110673165+Irina2304@users.noreply.github.com>
2024-02-11 15:05:48 +0100

    h-scroll
    горизонтальный скролл с отступом

--- src/components/ScreenPage/MainDashboard/MainDashboard.jsx
+++ src/components/ScreenPage/MainDashboard/MainDashboard.jsx
@@ -1,34 +1,46 @@
 /** @format */
 
 import React, { useState } from 'react';
-import { BoardContainer, BtnAddColumn, SvagAddColumn, StyledIconAdd } from './MainDashboard.styled';
+import {
+  BoardContainer,
+  BtnAddColumn,
+  SvagAddColumn,
+  StyledIconAdd,
+  WrapperBoardContainer,
+} from './MainDashboard.styled';
 import { AddColumnModal } from 'components/Modal';
 import { Columns } from '../Columns/Columns';
 import { useParams } from 'react-router-dom';
 import { useColumns } from 'hooks';
 
 const MainDashboard = () => {
-	const [isModalOpen, setIsModalOpen] = useState(false);
-	const { board } = useParams();
-	const { allColumns } = useColumns();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const { board } = useParams();
+  const { allColumns } = useColumns();
 
-	const handleOpenModal = () => {
-		setIsModalOpen(true);
-	};
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
 
-	return (
-		<BoardContainer>
-			<Columns board={board}>
-				<BtnAddColumn type='button' onClick={handleOpenModal}>
-					<StyledIconAdd>
-						<SvagAddColumn name='plus' />
-					</StyledIconAdd>
-					<span>Add {allColumns.length ? 'another' : null} column</span>
-				</BtnAddColumn>
-			</Columns>
-			<AddColumnModal board={board} isOpen={isModalOpen} setIsOpen={setIsModalOpen} />
-		</BoardContainer>
-	);
+  return (
+    <WrapperBoardContainer>
+      <BoardContainer>
+        <Columns board={board}>
+          <BtnAddColumn type="button" onClick={handleOpenModal}>
+            <StyledIconAdd>
+              <SvagAddColumn name="plus" />
+            </StyledIconAdd>
+            <span>Add {allColumns.length ? 'another' : null} column</span>
+          </BtnAddColumn>
+        </Columns>
+        <AddColumnModal
+          board={board}
+          isOpen={isModalOpen}
+          setIsOpen={setIsModalOpen}
+        />
+      </BoardContainer>
+    </WrapperBoardContainer>
+  );
 };
 
 export default MainDashboard;
