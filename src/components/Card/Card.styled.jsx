318e9f2de0836e78d2274f3841315582c86c3cf9
Irina2304 <110673165+Irina2304@users.noreply.github.com>
2024-02-11 22:19:17 +0100

    border-radius-card, text-decoration - link

--- src/components/Card/Card.styled.jsx
+++ src/components/Card/Card.styled.jsx
@@ -6,8 +6,9 @@ export const CardContainer = styled.div`
   height: 154px;
   display: flex;
   flex-direction: row;
-  background-color: ${props => props.theme.profile.background};;
-  border-radius: 8px solid rgba(17, 17, 17, 0.1);
+  background-color: ${props => props.theme.profile.background};
+  border-radius: 7px;
+  overflow: hidden;
 `;
 
 export const PriorityLine = styled.div`
--- src/components/Sidebar/BoardItem/BoardItem.jsx
+++ src/components/Sidebar/BoardItem/BoardItem.jsx
@@ -2,14 +2,14 @@
 
 import React, { useContext, useEffect } from 'react';
 import {
-	BoardContainer,
-	BoardNameContainer,
-	IconsContainer,
-	BoardIcon,
-	BoardText,
-	EditIcon,
-	DelIcon,
-	BoardLine,
+  BoardContainer,
+  BoardNameContainer,
+  IconsContainer,
+  BoardIcon,
+  BoardText,
+  EditIcon,
+  DelIcon,
+  BoardLine,
 } from './BoardItem.styled';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
@@ -19,55 +19,58 @@ import { useBoards } from 'hooks';
 import { MainContext } from 'components/Helpers';
 
 export const BoardItem = ({ nameBoard, boardId }) => {
-	const dispatch = useDispatch();
-	const navigate = useNavigate();
-	const { board } = useParams();
-	const { allBoards } = useBoards();
-	const { setIsOpenAddBoard, setBoardEdit } = useContext(MainContext);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { board } = useParams();
+  const { allBoards } = useBoards();
+  const { setIsOpenAddBoard, setBoardEdit } = useContext(MainContext);
 
-	useEffect(() => {
-		navigate(`/todos/${allBoards[0]._id}`);
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [allBoards]);
-	const getById = boardId => {
-		dispatch(getBoardById(boardId));
-	};
-	const handlerEditBoard = boardId => {
-		setIsOpenAddBoard(true);
-		setBoardEdit(boardId);
-	};
-	const handlerDelBoard = boardId => {
-		dispatch(delBoard(boardId));
-		
-	};
-	
-	const status = board === boardId;
+  useEffect(() => {
+    navigate(`/todos/${allBoards[0]._id}`);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [allBoards]);
+  const getById = boardId => {
+    dispatch(getBoardById(boardId));
+  };
+  const handlerEditBoard = boardId => {
+    setIsOpenAddBoard(true);
+    setBoardEdit(boardId);
+  };
+  const handlerDelBoard = boardId => {
+    dispatch(delBoard(boardId));
+  };
 
-	return (
-		<Link to={`/todos/${boardId}`} onClick={() => getById(boardId)}>
-			<StyleSheetManager shouldForwardProp={prop => prop !== 'active'}>
-				<BoardContainer active={status}>
-					<BoardNameContainer>
-						<BoardIcon name='type-6' active={status} />
-						<BoardText active={status}>{nameBoard}</BoardText>
-					</BoardNameContainer>
+  const status = board === boardId;
 
-					<IconsContainer active={status}>
-						<EditIcon
-							name='edit'
-							type='button'
-							onClick={() => handlerEditBoard(boardId)}
-						/>
-						<DelIcon
-							name='delete'
-							type='button'
-							onClick={() => handlerDelBoard(boardId)}
-						/>
-					</IconsContainer>
+  return (
+    <Link
+      to={`/todos/${boardId}`}
+      onClick={() => getById(boardId)}
+      style={{ textDecoration: 'none' }}
+    >
+      <StyleSheetManager shouldForwardProp={prop => prop !== 'active'}>
+        <BoardContainer active={status}>
+          <BoardNameContainer>
+            <BoardIcon name="type-6" active={status} />
+            <BoardText active={status}>{nameBoard}</BoardText>
+          </BoardNameContainer>
 
-					<BoardLine active={status} />
-				</BoardContainer>
-			</StyleSheetManager>
-		</Link>
-	);
+          <IconsContainer active={status}>
+            <EditIcon
+              name="edit"
+              type="button"
+              onClick={() => handlerEditBoard(boardId)}
+            />
+            <DelIcon
+              name="delete"
+              type="button"
+              onClick={() => handlerDelBoard(boardId)}
+            />
+          </IconsContainer>
+
+          <BoardLine active={status} />
+        </BoardContainer>
+      </StyleSheetManager>
+    </Link>
+  );
 };
