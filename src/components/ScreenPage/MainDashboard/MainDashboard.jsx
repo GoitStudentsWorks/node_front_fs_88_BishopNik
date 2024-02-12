612ab9b0fb8c1e5091cd6ea451fe9d9a7b62d6e1
Makorishko <129119228+Makorishko@users.noreply.github.com>
2024-02-12 11:12:30 +0100

    fix bcg

--- src/components/ScreenPage/MainDashboard/MainDashboard.jsx
+++ src/components/ScreenPage/MainDashboard/MainDashboard.jsx
@@ -1,47 +1,45 @@
 /** @format */
 
 import React, { useState } from 'react';
-import { StyleSheetManager } from 'styled-components';
+// import { StyleSheetManager } from 'styled-components';
 import {
-	BoardContainer,
-	BtnAddColumn,
-	SvagAddColumn,
-	StyledIconAdd,
-	// WrapperBoardContainer,
+  BoardContainer,
+  BtnAddColumn,
+  SvagAddColumn,
+  StyledIconAdd,
+  // WrapperBoardContainer,
 } from './MainDashboard.styled';
 import { AddColumnModal } from 'components/Modal';
 import { Columns } from '../Columns';
 import { useParams } from 'react-router-dom';
-import { useColumns, useBoards } from 'hooks';
+import { useColumns } from 'hooks';
 
 const MainDashboard = () => {
-	const [isModalOpen, setIsModalOpen] = useState(false);
-	const { board } = useParams();
-	const { allColumns } = useColumns();
-	const { allBoards } = useBoards();
-	const selectedBoard = allBoards.find(item => item._id === board);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const { board } = useParams();
+  const { allColumns } = useColumns();
 
-	const handleOpenModal = () => {
-		setIsModalOpen(true);
-	};
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
 
-	return (
-		<StyleSheetManager shouldForwardProp={prop => prop !== 'backgroundId'}>
-			{/* <WrapperBoardContainer> */}
-			<BoardContainer backgroundId={selectedBoard?.background}>
-				<Columns board={board}>
-					<BtnAddColumn type='button' onClick={handleOpenModal}>
-						<StyledIconAdd>
-							<SvagAddColumn name='plus' />
-						</StyledIconAdd>
-						<span>Add {allColumns.length ? 'another' : null} column</span>
-					</BtnAddColumn>
-				</Columns>
-				<AddColumnModal board={board} isOpen={isModalOpen} setIsOpen={setIsModalOpen} />
-			</BoardContainer>
-			{/* </WrapperBoardContainer> */}
-		</StyleSheetManager>
-	);
+  return (
+    <BoardContainer>
+      <Columns board={board}>
+        <BtnAddColumn type="button" onClick={handleOpenModal}>
+          <StyledIconAdd>
+            <SvagAddColumn name="plus" />
+          </StyledIconAdd>
+          <span>Add {allColumns.length ? 'another' : null} column</span>
+        </BtnAddColumn>
+      </Columns>
+      <AddColumnModal
+        board={board}
+        isOpen={isModalOpen}
+        setIsOpen={setIsModalOpen}
+      />
+    </BoardContainer>
+  );
 };
 
 export default MainDashboard;
--- src/components/styled.component/MainTodosPage.styled.jsx
+++ src/components/styled.component/MainTodosPage.styled.jsx
@@ -1,6 +1,8 @@
 /** @format */
 
 import styled from 'styled-components';
+import background from '../../img/background.json';
+
 
 export const Container = styled.div`
 	margin-left: auto;
@@ -66,7 +68,8 @@ export const Main = styled.main`
 	flex-direction: column;
 	position: relative;
 	grid-area: m;
-	background-color: ${props => props.theme.header.containerBackground};
+	background: ${props => props.backgroundId ? `url(${background.mobile[`image${props.backgroundId}`]})` : props.theme.header.containerBackground};
+	background-size: cover;
 	min-width: 320px;
 	height: calc(100vh - 60px);
 	padding-top: 14px;
@@ -82,11 +85,15 @@ export const Main = styled.main`
 		padding-top: 26px;
 		padding-bottom: 32px;
 		gap: 26px;
+		background: ${props => `url(${background.table[`image${props.backgroundId}`]})`};
+		background-size: cover;
 	}
 
 	@media screen and (min-width: 1440px) {
 		padding-top: 10px;
 		padding-bottom: 8px;
 		gap: 10px;
+		background: ${props => `url(${background.desktop[`image${props.backgroundId}`]})`};
+		background-size: cover;
 	}
 `;
