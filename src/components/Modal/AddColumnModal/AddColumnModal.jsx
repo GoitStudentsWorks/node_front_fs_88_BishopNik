49ebc555608b63e47aab374a66ed8c77a22e445e
Andrew <a.nikanorov@gmail.com>
2024-02-15 23:50:38 +0100

    Update AddColumnModal.jsx

--- src/components/Modal/AddColumnModal/AddColumnModal.jsx
+++ src/components/Modal/AddColumnModal/AddColumnModal.jsx
@@ -1,20 +1,20 @@
 /** @format */
 
-import React, { useContext, useState, useEffect, useCallback } from 'react';
+import React, { useContext, useEffect, useCallback } from 'react';
 import { Formik } from 'formik';
 import ModalWindow from '../Modal';
 import { customStyles } from '../Modal.styled';
 
 import {
-  ModalTitle,
-  StyledForm,
-  StyledFormField,
-  BtnAdd,
-  HeaderContainer,
-  CloseIcon,
-  IconWrapper,
-  AddIcon,
-  StyledErrorMessage,
+	ModalTitle,
+	StyledForm,
+	StyledFormField,
+	BtnAdd,
+	HeaderContainer,
+	CloseIcon,
+	IconWrapper,
+	AddIcon,
+	StyledErrorMessage,
 } from './AddColumnModal.styled';
 import { useDispatch } from 'react-redux';
 import { addColumn, updateColumn } from 'redux/columns/operations';
@@ -24,69 +24,60 @@ import { resetError } from 'redux/columns/columnsSlice';
 import { useColumns } from 'hooks';
 
 export const AddColumnModal = ({ board }) => {
-  const dispatch = useDispatch();
-  const { isOpenAddColumn, setIsOpenAddColumn, columnEdit, setColumnEdit } =
-    useContext(MainContext);
+	const dispatch = useDispatch();
+	const { isOpenAddColumn, setIsOpenAddColumn, columnEdit, setColumnEdit } =
+		useContext(MainContext);
 
-  const { isOpen } = useColumns();
+	const { isOpen } = useColumns();
 
-  const handleFormSubmit = values => {
-    if (columnEdit?._id) {
-      dispatch(updateColumn({ ...values, id: columnEdit?._id }));
-    } else {
-      dispatch(addColumn({ ...values }));
-    }
-  };
+	const handleFormSubmit = values => {
+		if (columnEdit?._id) {
+			dispatch(updateColumn({ ...values, id: columnEdit?._id }));
+		} else {
+			dispatch(addColumn({ ...values }));
+		}
+	};
 
-  const onClose = useCallback(() => {
-    setIsOpenAddColumn(false);
-    setColumnEdit(null);
-  }, [setColumnEdit, setIsOpenAddColumn]);
+	const onClose = useCallback(() => {
+		setIsOpenAddColumn(false);
+		setColumnEdit(null);
+	}, [setColumnEdit, setIsOpenAddColumn]);
 
-  useEffect(() => {
-    if (!isOpen) {
-      onClose();
-      dispatch(resetError());
-    }
-  }, [dispatch, isOpen, onClose]);
+	useEffect(() => {
+		if (!isOpen) {
+			onClose();
+			dispatch(resetError());
+		}
+	}, [dispatch, isOpen, onClose]);
 
-  return (
-    <ModalWindow
-      isOpen={isOpenAddColumn}
-      onRequestClose={onClose}
-      style={customStyles}
-    >
-      <Formik
-        initialValues={{
-          name: columnEdit?.name || '',
-          boardId: board,
-        }}
-        validationSchema={columnSchema}
-        onSubmit={handleFormSubmit}
-      >
-        {({ isSubmitting }) => (
-          <StyledForm autoComplete="off">
-            <HeaderContainer>
-              <ModalTitle>{columnEdit?._id ? 'Edit' : 'Add'}</ModalTitle>
-              <CloseIcon name="close" onClick={onClose} />
-            </HeaderContainer>
+	return (
+		<ModalWindow isOpen={isOpenAddColumn} onRequestClose={onClose} style={customStyles}>
+			<Formik
+				initialValues={{
+					name: columnEdit?.name || '',
+					boardId: board,
+				}}
+				validationSchema={columnSchema}
+				onSubmit={handleFormSubmit}
+			>
+				{({ isSubmitting }) => (
+					<StyledForm autoComplete='off'>
+						<HeaderContainer>
+							<ModalTitle>{columnEdit?._id ? 'Edit' : 'Add'}</ModalTitle>
+							<CloseIcon name='close' onClick={onClose} />
+						</HeaderContainer>
 
-            <StyledFormField
-              type="text"
-              name="name"
-              placeholder="Title"
-              autoFocus
-            />
-            <StyledErrorMessage name="name" component="div" />
-            <BtnAdd type="submit" disabled={isSubmitting}>
-              <IconWrapper>
-                <AddIcon name="plus" />
-              </IconWrapper>
-              {columnEdit?._id ? 'Edit' : 'Add'}
-            </BtnAdd>
-          </StyledForm>
-        )}
-      </Formik>
-    </ModalWindow>
-  );
+						<StyledFormField type='text' name='name' placeholder='Title' autoFocus />
+						<StyledErrorMessage name='name' component='div' />
+						<BtnAdd type='submit' disabled={isSubmitting}>
+							<IconWrapper>
+								<AddIcon name='plus' />
+							</IconWrapper>
+							{columnEdit?._id ? 'Edit' : 'Add'}
+						</BtnAdd>
+					</StyledForm>
+				)}
+			</Formik>
+		</ModalWindow>
+	);
 };
