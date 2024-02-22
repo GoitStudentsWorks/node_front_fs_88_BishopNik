7126431151f595c917aa9148b279d8feaefd12a3
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2024-02-22 17:59:59 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<Provider store={store}>
 			<PersistGate loading={null} persistor={persistor}>
-				<BrowserRouter basename='/'>
+				<BrowserRouter basename='/node_front_fs_88_BishopNik'>
 					<Context>
 						<СhangeThemeProvider>
 							<App />
